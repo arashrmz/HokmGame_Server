a3d9425ca85712555f0274a74e05444b16eabd01
Arash Ramzani <arash1.ramzani@gmail.com>
2022-06-25 21:51:40 +0430

    add FriendService class

---
+++ Services/FriendService.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HokmGame_Server.Data;
+
+namespace HokmGame_Server.Services;
+
+public class FriendService
+{
+    private AppDbContext _context;
+
+    public FriendService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddFriend(User sender, User friend)
+    {
+        var friendship = new Friendship
+        {
+            SenderId = sender.Id,
+            FriendId = friend.Id
+        };
+        await _context.Friendships.AddAsync(friendship);
+        await _context.SaveChangesAsync();
+    }
+}
