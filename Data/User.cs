43ea833d7b9ba28de045a4431a4d8609260fbbf1
Arash Ramzani <arash1.ramzani@gmail.com>
2022-06-27 16:25:49 +0430

    remove friends list

--- Data/User.cs
+++ Data/User.cs
@@ -5,5 +5,4 @@ public class User
     public int Id { get; set; }
     public string Name { get; set; }
     public string Password { get; set; }
-    public virtual ICollection<Friendship> Friends { get; set; }
 }
